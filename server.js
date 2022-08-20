0dd0161276370825fad733d93da548cc2da941ab
Manmeet Bhogal <manmeetbhogal99@gmail.com>
2022-08-20 13:20:51 -0400

    Authorization key for Ballchasing API & test API call

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
 const express = require("express");
+const https = require("https");
+
+// Authorization key for Ballchasing API Token
+const fs = require("fs");
+var key = fs.readFileSync("./key.txt").toString();
+
 const app = express();
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+
+    const options = {
+        hostname: 'ballchasing.com',
+        path: '/api/replays/69055622-3708-42ee-b222-964e432f3d67',
+        headers: {
+            Authorization: key
+        }
+    };
+
+    https.get(options, function(response){
+
+        console.log("Response statusCode: " + response.statusCode);
+
+        let data = '';
+        response.on("data", chunk => {
+            data += chunk;
+        });
+        response.on("end", () =>{
+            const apiData = JSON.parse(data);
+            console.log(apiData);
+            const player0Camera = apiData.blue.players[0].camera;
+            res.send(player0Camera);
+        })
+
+        // console.log("Response statusCode: " + response.statusCode);
+
+        // response.on("data", function(data){
+        //     const apiData = JSON.parse(data);
+        //     console.log(apiData);
+        // })
+    })
+
+    // res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function(){
