ce20852c9a1321f2e8a79e2f67ed6909d2a8c190
rydrinkswater <niny49@sbcglobal.net>
2022-08-20 21:59:04 -0500

    Quick outline for getting down to the player info with for loops. Definitely could be an easier way to accomplish this but this was first thing I came up with.

--- server.js
+++ server.js
@@ -7,27 +7,43 @@ var key = fs.readFileSync("./key.txt").toString();
 
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
-    const axiosInstance = axios.create({
-        baseURL: "https://ballchasing.com/api"
-      })
+  const axiosInstance = axios.create({
+    baseURL: "https://ballchasing.com/api"
+  })
 
-      axiosInstance.get('/replays/?uploader=76561199225615730', {
-        headers: {
-          Authorization: key
+  axiosInstance.get('/replays/?uploader=76561199225615730', {
+      headers: {
+        Authorization: key
+      }
+    }).then(response => {
+      data = response.data;
+      list = data.list;
+      for (value in list) {
+        var blueTeam = list[value].blue;
+        for (value in blueTeam) {
+          var blueName = blueTeam.name;
+          var bluePlayers = blueTeam.players;
+          console.log(blueName + "\n");
+          for (player in bluePlayers) {
+            var playerName = bluePlayers[player].name;
+            var playerID = bluePlayers[player].id;
+            console.log(playerName + "\n");
+            console.log(playerID.platform);
+            console.log(playerID.id + "\n");
+          }
+          console.log("\n");
         }
-      }).then(response => {
-        data = response.data;
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
